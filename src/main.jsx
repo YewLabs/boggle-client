2d667248e61afeec0420e696933232858b6c4859
krawthekrow <krawthekrow@gmail.com>
2021-01-03 15:10:48 -0500

    react skeleton

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+class Main extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <div>Test</div>;
+	}
+};
+
+const domContainer = document.querySelector('#mainContainer');
+ReactDOM.render(<Main/>, domContainer);
