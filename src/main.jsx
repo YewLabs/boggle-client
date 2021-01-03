825d7f28484ee9c80fa575baf50146807424e518
CJ Quines <cj@cjquines.com>
2021-01-03 16:39:21 -0500

    reorg

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      navigation: "mainmenu",
+    };
   }
+
+  navigate = (target) => {
+    this.setState({
+      navigation: target,
+    });
+  };
+
   render() {
-    return <div>Test</div>;
+    switch (this.state.navigation) {
+      case "mainmenu":
+        return (
+          <div>
+            Test <button onClick={(e) => this.navigate("xxy")}>asdf</button>
+          </div>
+        );
+      case "xxy":
+        return <div>xxy</div>;
+      default:
+        return <div>Loading...</div>;
+    }
   }
 }
 
