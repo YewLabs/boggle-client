82d9054be8ce77025330d20170d474444fa3bef5
krawthekrow <krawthekrow@gmail.com>
2021-01-09 17:56:39 -0500

    change achievement thresholds, swap 3 and 4

--- src/data.js
+++ src/data.js
@@ -39,32 +39,6 @@ export const data = [
     { x: 4, y: 2, cx: 256.0, cy: 302.0 },
   ],
   [
-    { x: 0, y: 2, cx: 32.0, cy: 160.0 },
-    { x: 0, y: 3, cx: 32.0, cy: 224.0 },
-    { x: 1, y: 1, cx: 96.0, cy: 96.0 },
-    { x: 1, y: 2, cx: 96.0, cy: 160.0 },
-    { x: 1, y: 3, cx: 96.0, cy: 224.0 },
-    { x: 1, y: 4, cx: 96.0, cy: 288.0 },
-    { x: 2, y: 0, cx: 160.0, cy: 32.0 },
-    { x: 2, y: 1, cx: 160.0, cy: 96.0 },
-    { x: 2, y: 2, cx: 160.0, cy: 160.0 },
-    { x: 2, y: 3, cx: 160.0, cy: 224.0 },
-    { x: 2, y: 4, cx: 160.0, cy: 288.0 },
-    { x: 2, y: 5, cx: 160.0, cy: 352.0 },
-    { x: 3, y: 0, cx: 224.0, cy: 32.0 },
-    { x: 3, y: 1, cx: 224.0, cy: 96.0 },
-    { x: 3, y: 2, cx: 224.0, cy: 160.0 },
-    { x: 3, y: 3, cx: 224.0, cy: 224.0 },
-    { x: 3, y: 4, cx: 224.0, cy: 288.0 },
-    { x: 3, y: 5, cx: 224.0, cy: 352.0 },
-    { x: 4, y: 1, cx: 288.0, cy: 96.0 },
-    { x: 4, y: 2, cx: 288.0, cy: 160.0 },
-    { x: 4, y: 3, cx: 288.0, cy: 224.0 },
-    { x: 4, y: 4, cx: 288.0, cy: 288.0 },
-    { x: 5, y: 2, cx: 352.0, cy: 160.0 },
-    { x: 5, y: 3, cx: 352.0, cy: 224.0 },
-  ],
-  [
     { x: 0, y: 0, z: 0, cx: 64.0, cy: 32.0 },
     { x: 0, y: 0, z: 1, cx: 128.0, cy: 224.0 },
     { x: 0, y: 0, z: 2, cx: 192.0, cy: 416.0 },
@@ -93,6 +67,32 @@ export const data = [
     { x: 2, y: 2, z: 1, cx: 256.0, cy: 352.0 },
     { x: 2, y: 2, z: 2, cx: 320.0, cy: 544.0 },
   ],
+  [
+    { x: 0, y: 2, cx: 32.0, cy: 160.0 },
+    { x: 0, y: 3, cx: 32.0, cy: 224.0 },
+    { x: 1, y: 1, cx: 96.0, cy: 96.0 },
+    { x: 1, y: 2, cx: 96.0, cy: 160.0 },
+    { x: 1, y: 3, cx: 96.0, cy: 224.0 },
+    { x: 1, y: 4, cx: 96.0, cy: 288.0 },
+    { x: 2, y: 0, cx: 160.0, cy: 32.0 },
+    { x: 2, y: 1, cx: 160.0, cy: 96.0 },
+    { x: 2, y: 2, cx: 160.0, cy: 160.0 },
+    { x: 2, y: 3, cx: 160.0, cy: 224.0 },
+    { x: 2, y: 4, cx: 160.0, cy: 288.0 },
+    { x: 2, y: 5, cx: 160.0, cy: 352.0 },
+    { x: 3, y: 0, cx: 224.0, cy: 32.0 },
+    { x: 3, y: 1, cx: 224.0, cy: 96.0 },
+    { x: 3, y: 2, cx: 224.0, cy: 160.0 },
+    { x: 3, y: 3, cx: 224.0, cy: 224.0 },
+    { x: 3, y: 4, cx: 224.0, cy: 288.0 },
+    { x: 3, y: 5, cx: 224.0, cy: 352.0 },
+    { x: 4, y: 1, cx: 288.0, cy: 96.0 },
+    { x: 4, y: 2, cx: 288.0, cy: 160.0 },
+    { x: 4, y: 3, cx: 288.0, cy: 224.0 },
+    { x: 4, y: 4, cx: 288.0, cy: 288.0 },
+    { x: 5, y: 2, cx: 352.0, cy: 160.0 },
+    { x: 5, y: 3, cx: 352.0, cy: 224.0 },
+  ],
 ];
 
 export const adjacent = [
@@ -100,39 +100,39 @@ export const adjacent = [
     Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1,
   ({ x: x1, y: y1 }) => ({ x: x2, y: y2 }) =>
     Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1 && x2 - x1 != y2 - y1,
+  ({ x: x1, y: y1, z: z1 }) => ({ x: x2, y: y2, z: z2 }) =>
+    Math.abs(x1 - x2) + Math.abs(y1 - y2) + Math.abs(z1 - z2) === 1,
   ({ x: x1, y: y1 }) => ({ x: x2, y: y2 }) => {
     const dx = Math.abs(x1 - x2);
     const dy = Math.abs(y1 - y2);
     return Math.abs(dx - dy) == 1 && dx + dy == 3;
   },
-  ({ x: x1, y: y1, z: z1 }) => ({ x: x2, y: y2, z: z2 }) =>
-    Math.abs(x1 - x2) + Math.abs(y1 - y2) + Math.abs(z1 - z2) === 1,
 ];
 
 export const trophyDescriptions = [
   [
     "Get 30% words in level 1",
     "Get 30% points in level 1",
-    "Get 60% words in level 1",
-    "Get 60% points in level 1",
+    "Get 50% words in level 1",
+    "Get 50% points in level 1",
   ],
   [
     "Get 30% words in level 2",
     "Get 30% points in level 2",
-    "Get 60% words in level 2",
-    "Get 60% points in level 2",
+    "Get 50% words in level 2",
+    "Get 50% points in level 2",
   ],
   [
-    "Get 30% words in level 3",
-    "Get 30% points in level 3",
-    "Get 60% words in level 3",
-    "Get 60% points in level 3",
+    "Get 25% words in level 4",
+    "Get 25% points in level 4",
+    "Get 45% words in level 4",
+    "Get 45% points in level 4",
   ],
   [
-    "Get 30% words in level 4",
-    "Get 30% points in level 4",
-    "Get 60% words in level 4",
-    "Get 60% points in level 4",
+    "Get 20% words in level 3",
+    "Get 20% points in level 3",
+    "Get 35% words in level 3",
+    "Get 35% points in level 3",
   ],
 ];
 
